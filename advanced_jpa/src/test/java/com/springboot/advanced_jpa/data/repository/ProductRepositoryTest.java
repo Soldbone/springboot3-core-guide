0f414f00f4a534a7fceafaecd498d2dd906f8bd7
Soldbone <1472ezs@gmail.com>
2025-08-13 15:38:41 +0900

    [Ch08 - Advanced JPA] Add omitted contents that is related to QueryDSL

--- advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
+++ advanced_jpa/src/test/java/com/springboot/advanced_jpa/data/repository/ProductRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.springboot.advanced_jpa.data.repository;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springboot.advanced_jpa.data.entity.Product;
 import com.springboot.advanced_jpa.data.entity.QProduct;
 import jakarta.persistence.EntityManager;
@@ -94,15 +96,93 @@ public class ProductRepositoryTest {
     }
 
     @Test
-    public void auditingTest() {
-        Product product = new Product();
-        product.setName("펜");
-        product.setPrice(1000);
-        product.setStock(100);
+    void queryDslTest2() {
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        QProduct qProduct = QProduct.product;
 
-        Product savedProduct = productRepository.save(product);
+        List<Product> productList = jpaQueryFactory.selectFrom(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
 
-        System.out.println("productName : " + savedProduct.getName());
-        System.out.println("createdAt : " + savedProduct.getCreatedAt());
+        for (Product product : productList) {
+            System.out.println("----------------");
+            System.out.println();
+            System.out.println("Product Number : " + product.getNumber());
+            System.out.println("Product Name : " + product.getName());
+            System.out.println("Product Price : " + product.getPrice());
+            System.out.println("Product Stock : " + product.getStock());
+            System.out.println();
+            System.out.println("----------------");
+        }
     }
+
+    @Test
+    void queryDslTest3() {
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<String> productList = jpaQueryFactory
+                .select(qProduct.name)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (String product : productList) {
+            System.out.println("---------------");
+            System.out.println("Product Name : " + product);
+            System.out.println("---------------");
+        }
+
+        List<Tuple> tupleList = jpaQueryFactory
+                .select(qProduct.name, qProduct.price)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (Tuple product : tupleList) {
+            System.out.println("---------------");
+            System.out.println("Product Name : " + product.get(qProduct.name));
+            System.out.println("Product Price : " + product.get(qProduct.price));
+            System.out.println("---------------");
+        }
+    }
+
+    @Autowired
+    JPAQueryFactory jpaQueryFactory;
+
+    @Test
+    void queryDslTest4() {
+        QProduct qProduct = QProduct.product;
+
+        List<String> productList = jpaQueryFactory
+                .select(qProduct.name)
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch();
+
+        for (String product : productList) {
+            System.out.println("---------------");
+            System.out.println("Product Name : " + product);
+            System.out.println("---------------");
+        }
+
+    }
+
+
+//    @Test
+//    public void auditingTest() {
+//        Product product = new Product();
+//        product.setName("펜");
+//        product.setPrice(1000);
+//        product.setStock(100);
+//
+//        Product savedProduct = productRepository.save(product);
+//
+//        System.out.println("productName : " + savedProduct.getName());
+//        System.out.println("createdAt : " + savedProduct.getCreatedAt());
+//    }
 }
