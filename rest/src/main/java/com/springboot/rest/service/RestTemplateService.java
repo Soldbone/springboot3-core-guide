81ef30866e258404a328d827c22e6708b26443a1
Easyseob <1472ezs@gmail.com>
2025-08-20 12:31:34 +0900

    [Ch12] Backup code

---
+++ rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
@@ -0,0 +1,27 @@
+package com.springboot.rest.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+    public String getName() {
+        URI uri = UriComponentsBuilder.
+                fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+}
--- serverBox/src/main/java/com/springboot/serverbox/dto/MemberDto.java
+++ serverBox/src/main/java/com/springboot/serverbox/dto/MemberDto.java
@@ -6,5 +6,37 @@ public class MemberDto {
     private String email;
     private String organization;
 
-    public String
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOrganization() {
+        return this.organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberDto{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", organization='" + organization +'\'' +
+                '}';
+    }
+
 }
