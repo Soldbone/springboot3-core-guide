5c12d031f3a2ad50b162a958b1f8e36ade866088
Soldbone <1472ezs@gmail.com>
2025-08-21 16:43:32 +0900

    [Ch12] Inter Server Communication w/ RestTemplate, WebClient, RestTemplate

--- rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
+++ rest/src/main/java/com/springboot/rest/service/RestTemplateService.java
@@ -1,6 +1,13 @@
 package com.springboot.rest.service;
 
+import com.springboot.rest.dto.MemberDto;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -23,5 +30,101 @@ public class RestTemplateService {
 
         return responseEntity.getBody();
     }
+
+    public String getNameWithPathVariable() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api/{name}")
+                .encode()
+                .build()
+                .expand("Flature")
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithParameter() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api/{name}")
+                .queryParam("name", "Flature")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public ResponseEntity<MemberDto> postWithParamAndBody() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api")
+                .queryParam("name", "Flature")
+                .queryParam("email", "flature@wikibooks.co.kr")
+                .queryParam("organization", "Wikibooks")
+                .encode()
+                .build()
+                .toUri();
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("flature!!");
+        memberDto.setEmail("flature@gmail.com");
+        memberDto.setOrganization("Around Hub Studio");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<MemberDto> responseEntity = restTemplate.postForEntity(uri, memberDto, MemberDto.class);
+
+        return responseEntity;
+    }
+
+    public ResponseEntity<MemberDto> postWithHeader() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api/add-header")
+                .encode()
+                .build()
+                .toUri();
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("flature");
+        memberDto.setEmail("flature@wikibooks.co.kr");
+        memberDto.setOrganization("Around Hub Studio))");
+
+        RequestEntity<MemberDto> requestEntity = RequestEntity
+                .post(uri)
+                .header("my-header", "Wikibooks API")
+                .body(memberDto);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<MemberDto> responseEntity = restTemplate.exchange(requestEntity, MemberDto.class);
+
+        return responseEntity;
+    }
+
+    // # RestTemplate Custom 하는 부분
+//    public RestTemplate restTemplate() {
+//        var connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+//                .setMaxConnTotal(500)       // 전체 커넥션 수
+//                .setMaxConnPerRoute(500)    // 라우트(호스트)별 커넥션 수
+//                .build();
+//
+//        // HttpClient 생성
+//        CloseableHttpClient httpClient = HttpClients.custom()
+//                .setConnectionManager(connectionManager)
+//                .build();
+//
+//        // Spring RestTemplate에서 사용할 RequestFactory
+//        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//        factory.setConnectTimeout((int) Timeout.ofMilliseconds(2000).toMilliseconds()); // 연결 타임아웃
+//        factory.setReadTimeout((int) Timeout.ofMilliseconds(5000).toMilliseconds());   // 읽기 타임아웃
+//
+//        return new RestTemplate(factory);
+//    }
 
 }
